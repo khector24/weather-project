a98f4ff203cd20d54065eeed215d50bce85c083b
Kenny Hector <kennyhector24@gmail.com>
2025-05-20 21:17:03 -0400

    Update code to JS6.

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-require("dotenv").config();
+import dotenv from "dotenv";
 
-const express = require("express");
-const https = require("https");
-const bodyParser = require("body-parser");
+import express from "express";
+import https from "https";
+import bodyParser from "body-parser";
+
+dotenv.config();
 
 const app = express();
 app.set("view engine", "ejs");
@@ -10,45 +12,55 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
     res.render("index")
 });
 
-app.post("/", (req, res) => {
+app.post("/", async (req, res) => {
     const query = req.body.cityName;
     const apiKey = process.env.API_KEY;
     const unit = "imperial";
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${apiKey}&units=${unit}`;
 
-    https.get(url, (response) => {
-        response.on("data", (data) => {
-            const weatherData = JSON.parse(data);
+    try {
+        const response = await fetch(url);
+        const weatherData = await response.json();
 
-            if (weatherData.cod !== 200) {
-                return res.render("weather-response", {
-                    error: "City not found. Please try again.",
-                    temp: null,
-                    description: null,
-                    iconURL: null,
-                    city: query
-                });
-            }
-
-            const temp = weatherData.main.temp;
-            const description = weatherData.weather[0].description;
-            const icon = weatherData.weather[0].icon;
-            const iconURL = `https://openweathermap.org/img/wn/${icon}@2x.png`;
-
-            res.render("weather-response", {
-                temp,
-                description,
-                iconURL,
-                city: query,
-                error: null
+        if (weatherData.cod !== 200) {
+            return res.render("weather-response", {
+                error: "City not found. Please try again.",
+                temp: null,
+                description: null,
+                iconURL: null,
+                city: query
             });
+        }
+
+        const temp = weatherData.main.temp;
+        const description = weatherData.weather[0].description;
+        const icon = weatherData.weather[0].icon;
+        const iconURL = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+
+        res.render("weather-response", {
+            temp,
+            description,
+            iconURL,
+            city: query,
+            error: null
         });
-    });
+
+    } catch (err) {
+        console.error("Fetch error:", err);
+        res.render("weather-response", {
+            error: "Something went wrong. Please try again.",
+            temp: null,
+            description: null,
+            iconURL: null,
+            city: query
+        });
+    }
 });
+
 
 app.listen(3000, function () {
     console.log("Server is running on port 3000.")
