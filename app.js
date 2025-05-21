485fbcd20ed20192083b3ea30c935e9121202fcd
Kenny Hector <kennyhector24@gmail.com>
2025-05-20 21:01:00 -0400

    Update search again btn

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/index.html")
+    res.render("index")
 });
 
 app.post("/", (req, res) => {
